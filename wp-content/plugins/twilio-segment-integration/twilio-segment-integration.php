289613962b8d94699ccf38053c844efc1e911c10
Danh Thong <danhthong@gmail.com>
2023-09-06 22:05:32 +0700

    feat: add plugin to send page load data to segment

---
+++ wp-content/plugins/twilio-segment-integration/twilio-segment-integration.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Plugin Name: Twilio Segment Integration
+ * Plugin URI: https://www.abstractwp.com/
+ * Description: A WordPress plugin to integrate the Twilio Segment PHP SDK
+ * Version: 1.0.0
+ * Author: AbstractWP
+ * Author URI: https://www.example.com
+ * License: GPL-3.0
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: twilio-segment-integration
+ * Domain Path: /languages
+ */
+
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+define('SEGMENT_WRITE_KEY', 'h9Ac7yFl5POSCQhvI1HFZLsKeqBlGnwp'); // TODO: create settings page.
+
+use Segment\Client;
+
+class TwilioSegmentIntegration {
+		private $client;
+
+		public function __construct() {
+			$this->initialize();
+			$this->hooks();
+		}
+
+		public function initialize() {
+			$write_key = defined('SEGMENT_WRITE_KEY') ? SEGMENT_WRITE_KEY : '';
+
+			$this->client = new Client($write_key, [
+				'ssl' => true,
+			]);
+		}
+
+		public function hooks() {
+			add_action('init', [$this, 'load_textdomain']);
+			add_action('wp_footer', [$this, 'track_page_load']);
+		}
+
+		public function load_textdomain() {
+			load_plugin_textdomain('twilio-segment-integration', false, dirname(plugin_basename(__FILE__)) . '/languages');
+		}
+
+		public function track_page_load() {
+			if (is_user_logged_in()) {
+				$user   = wp_get_current_user();
+				$userId = $user->ID;
+			} else {
+				$userId = 'anonymous';
+			}
+
+			$page_title = get_the_title();
+			$page_url   = get_permalink();
+
+			if (is_archive()) {
+				$page_title = get_the_archive_title();
+			}
+
+			if (is_category() || is_tag()) {
+				$page_url = get_term_link( get_queried_object() );
+
+				$page_url = str_replace( "/category/thoughts/", "/thoughts/", $page_url );
+			}
+
+			if (is_post_type_archive()) {
+				$page_url = get_post_type_archive_link( get_post_type() );
+			}
+
+			$this->client->page([
+				'userId'     => $userId,
+				'name'       => $page_title,
+				'properties' => [
+					'title' => $page_title,
+					'url'   => $page_url,
+				],
+			]);
+		}
+}
+
+$twilio_segment_integration = new TwilioSegmentIntegration();
---
+++ wp-content/plugins/twilio-segment-integration/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f83b28e092f736569eee5b253932a83ef2188155',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f83b28e092f736569eee5b253932a83ef2188155',
+            'dev_requirement' => false,
+        ),
+        'segmentio/analytics-php' => array(
+            'pretty_version' => '3.6.0',
+            'version' => '3.6.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../segmentio/analytics-php',
+            'aliases' => array(),
+            'reference' => 'd7fd3d56c8e15e0e09dd5ccb6443e5008dcc4b72',
+            'dev_requirement' => false,
+        ),
+    ),
+);
