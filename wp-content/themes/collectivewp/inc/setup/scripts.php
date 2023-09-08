4731c1260448e0f79f1140f016b8280241c26c14
Danh Thong <danhthong@gmail.com>
2023-09-08 10:43:36 +0700

    fix: menu position on tablet

--- wp-content/themes/collectivewp/inc/setup/scripts.php
+++ wp-content/themes/collectivewp/inc/setup/scripts.php
@@ -19,7 +19,7 @@ function scripts() {
 		$asset_file = include $asset_file_path;
 	} else {
 		$asset_file = [
-			'version'      => '1.2.9',
+			'version'      => '1.3.0',
 			'dependencies' => [ 'wp-polyfill' ],
 		];
 	}
